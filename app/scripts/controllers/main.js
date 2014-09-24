f2b7f04ef3da8196362fed9a5feb3822af86b016
fgcui <fgcui1204@163.com>
2014-09-24 22:57:37 +0800

    annotation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('letusgo')
-  .controller('MainCtrl', function ($scope, $location, productService) {
-
-    productService.setSortToLocal();
-    productService.setToLocal();
+  .controller('MainCtrl', function ($scope, productService) {
+//
+//    productService.setSortToLocal();
+//    productService.setToLocal();
 
     $scope.totalCount = productService.getTotalCount();
   });
