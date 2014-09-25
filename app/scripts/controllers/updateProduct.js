cff5bb905f25a915968d078e0a54a244fb7b1a68
fgcui <fgcui1204@163.com>
2014-09-25 17:39:47 +0800

    modify getAllsorts

--- app/scripts/controllers/updateProduct.js
+++ app/scripts/controllers/updateProduct.js
@@ -1,12 +1,15 @@
 'use strict';
 angular.module('letusgo')
   .controller('updateProduct', function ($scope,fromLocal,productManagerService,$routeParams,$location) {
-    $scope.allSorts = productManagerService.getAllSort();
-    var item = productManagerService.getProductById($routeParams.barcode);
-    $scope.productInfo = {'barcode':item.barcode,'productSort':item.productSort,productName:item.productName,productPrice:item.productPrice,productUnit:item.productUnit};
 
-    $scope.doUpdate = function(){
-      productManagerService.doUpdate($scope.productInfo);
-      $location.path('/productManager');
-    };
+    productManagerService.getAllSorts(function(data){
+      $scope.allSorts = data;
+    });
+//    var item = productManagerService.getProductById($routeParams.barcode);
+//    $scope.productInfo = {'barcode':item.barcode,'productSort':item.productSort,productName:item.productName,productPrice:item.productPrice,productUnit:item.productUnit};
+//
+//    $scope.doUpdate = function(){
+//      productManagerService.doUpdate($scope.productInfo);
+//      $location.path('/productManager');
+//    };
   });
