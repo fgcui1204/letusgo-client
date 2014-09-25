421cd20fec0b3ab79983231e8d66843a223b8197
fgcui <fgcui1204@163.com>
2014-09-25 11:07:26 +0800

    annotation the unuse

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -1,16 +1,19 @@
 'use strict';
 angular.module('letusgo')
     .controller('CartCtrl', function ($scope,fromLocal,cartService,productService) {
-        $scope.cartItems = fromLocal.getData("cartProduct");
+       // $scope.cartItems = fromLocal.getData("cartProduct");
 
-        $scope.$parent.totalCount = productService.getTotalCount();
-
-        $scope.totalMoney = cartService.getTotalMoney();
-
-        $scope.changeCount = function (item) {
-            cartService.changeCount(item);
-            $scope.totalMoney = cartService.getTotalMoney();
-            $scope.$parent.totalCount = productService.getTotalCount();
-            $scope.cartItems = fromLocal.getData('cartProduct');
-        };
+    productService.cartItem(function(data){
+      $scope.cartItems = data;
+    });
+//        $scope.$parent.totalCount = productService.getTotalCount();
+//
+//        $scope.totalMoney = cartService.getTotalMoney();
+//
+//        $scope.changeCount = function (item) {
+//            cartService.changeCount(item);
+//            $scope.totalMoney = cartService.getTotalMoney();
+//            $scope.$parent.totalCount = productService.getTotalCount();
+//            $scope.cartItems = fromLocal.getData('cartProduct');
+//        };
     });
