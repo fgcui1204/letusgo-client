92ff4be380b77883dd4fe00c79b7055b271f4335
fgcui <fgcui1204@163.com>
2014-09-26 00:17:23 +0800

    show sorts list

--- app/scripts/controllers/sortManager.js
+++ app/scripts/controllers/sortManager.js
@@ -1,20 +1,22 @@
 'use strict';
 angular.module('letusgo')
   .controller('sortManagerCtrl', function ($scope, fromLocal, $location, sortManagerService) {
-    $scope.sorts = sortManagerService.getAllSorts();
-    $scope.addSort = function () {
-      var sort = sortManagerService.sortInfo();
-      sort.sname = $scope.sname;
-      sortManagerService.addSort(sort);
-      $location.path('/sortManager');
-    };
-
-    $scope.delete = function (sort) {
-      sortManagerService.delete(sort);
-      $scope.sorts = sortManagerService.getAllSorts();
-    };
-    $scope.toUpdate = function (sort) {
-      sortManagerService.toUpdate(sort);
-
-    };
+    sortManagerService.getAllSorts(function(data){
+      $scope.sorts = data;
+    });
+//    $scope.addSort = function () {
+//      var sort = sortManagerService.sortInfo();
+//      sort.sname = $scope.sname;
+//      sortManagerService.addSort(sort);
+//      $location.path('/sortManager');
+//    };
+//
+//    $scope.delete = function (sort) {
+//      sortManagerService.delete(sort);
+//      $scope.sorts = sortManagerService.getAllSorts();
+//    };
+//    $scope.toUpdate = function (sort) {
+//      sortManagerService.toUpdate(sort);
+//
+//    };
   });
