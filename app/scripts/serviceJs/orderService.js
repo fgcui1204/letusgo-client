2b74156ec436954343e316e0b10a081e2c7eb510
fgcui <fgcui1204@163.com>
2014-09-26 20:26:28 +0800

    modify remove

--- app/scripts/serviceJs/orderService.js
+++ app/scripts/serviceJs/orderService.js
@@ -1,8 +1,12 @@
 'use strict';
-angular.module('letusgo').service('orderService', function ($location,fromLocal) {
-    this.remove = function(){
-        localStorage.removeItem('cartProduct');
-        fromLocal.setData('totalCount',0);
-        $location.path('/homePage').replace();
+angular.module('letusgo').service('orderService', function ($location,fromLocal,$http) {
+    this.remove = function(callback){
+      $http.del('/api/payment').success(function(data){
+        console.log(data);
+        callback(data);
+      });
+//        localStorage.removeItem('cartProduct');
+//        fromLocal.setData('totalCount',0);
+//        $location.path('/homePage').replace();
     };
 });
