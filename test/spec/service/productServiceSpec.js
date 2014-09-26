b8dc42b6fbc5c7e86fd0ca5eb0d5b3239214901d
fgcui <fgcui1204@163.com>
2014-09-26 23:53:01 +0800

    test sorts

--- test/spec/service/productServiceSpec.js
+++ test/spec/service/productServiceSpec.js
@@ -45,6 +45,15 @@ describe('productService', function () {
       expect(items.length).toEqual(3);
     });
   });
+
+  it('it should test sorts', function () {
+    productService.sort(function(callback){
+
+      callback(sorts);
+      expect(sorts[0].sname).toEqual('水果');
+      expect(sorts.length).toEqual(4);
+    });
+  });
   xit('when cart is null,the total count in cart is one', function () {
     // spyOn(fromLocal,'getData');
     spyOn(fromLocal, 'getData').and.returnValue(null);
