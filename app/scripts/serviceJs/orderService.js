2c6742618c130eddea49ea22aef52c5d0e390b84
fgcui <fgcui1204@163.com>
2014-09-26 22:20:26 +0800

    add location.path

--- app/scripts/serviceJs/orderService.js
+++ app/scripts/serviceJs/orderService.js
@@ -1,12 +1,9 @@
 'use strict';
 angular.module('letusgo').service('orderService', function ($location,fromLocal,$http) {
     this.remove = function(callback){
-      $http.del('/api/payment').success(function(data){
-        console.log(data);
+      $http.delete('/api/payment').success(function(data){
         callback(data);
       });
-//        localStorage.removeItem('cartProduct');
-//        fromLocal.setData('totalCount',0);
-//        $location.path('/homePage').replace();
+        $location.path('/homePage').replace();
     };
 });
