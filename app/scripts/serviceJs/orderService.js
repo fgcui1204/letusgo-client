edb94dcbb2fe5b2f3bb7ef0c9530068accea2c04
fgcui <fgcui1204@163.com>
2014-09-28 08:58:36 +0800

    remove location

--- app/scripts/serviceJs/orderService.js
+++ app/scripts/serviceJs/orderService.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('letusgo').service('orderService', function ($location,fromLocal,$http) {
+angular.module('letusgo').service('orderService', function (fromLocal,$http) {
     this.remove = function(callback){
       $http.delete('/api/payment').success(function(data){
         callback(data);
