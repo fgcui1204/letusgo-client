44c7f1ce8fe07784d9021d71b9bd0560e839b3ea
fgcui <fgcui1204@163.com>
2014-10-01 22:44:41 +0800

    remove annotation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,7 @@
 
 angular.module('letusgo')
   .controller('MainCtrl', function ($scope, productService) {
-//
-//    productService.setSortToLocal();
-//    productService.setToLocal();
 
-    //$scope.totalCount = productService.getTotalCount();
     productService.getTotalCount(function (data) {
       $scope.$parent.totalCount = data;
     });
