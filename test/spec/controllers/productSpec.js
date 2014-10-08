e9d2c2b03489b49056c1e522f020ceafc60e36bf
fgcui <fgcui1204@163.com>
2014-10-08 23:00:02 +0800

    test get all products

--- test/spec/controllers/productSpec.js
+++ test/spec/controllers/productSpec.js
@@ -3,6 +3,7 @@
 describe('productCtrl',function(){
     var $scope,productService,createController,products,$controller;
     beforeEach(function () {
+
         module('letusgo');
         inject(function ($injector){
             $scope = $injector.get('$rootScope').$new();
@@ -10,26 +11,30 @@ describe('productCtrl',function(){
             $controller = $injector.get('$controller');
 
         });
+
             createController = function () {
             return $controller('ProCtrl', {
               $scope: $scope,
               productService: productService
             });
           };
+
       products = [
         {barcode: '4', category: {id: '2', name: '饮料'}, name: '雪碧', price: '3', unit: '瓶'},
         {barcode: '5', category: {id: '3', name: '服装'}, name: 'NIKE鞋', price: '300', unit: '双'}
       ];
+
+      spyOn(productService,'product').and.callFake(function(callback){
+        callback(products);
+      });
     });
-    it ('setToLocal should be execute', function () {
-      var allProduct = [{productSort:'水果',productName:'苹果',productPrice:'10',productUnit:'千克'},
-        {productSort:'水果',productName:'香蕉',productPrice:'5',productUnit:'千克'}];
-        spyOn(productService,'setToLocal');
-        spyOn(productService,'addToCart');
-        spyOn(fromLocal,'getData').and.returnValue(allProduct);
-        createController();
-        $scope.addToCart(allProduct[0]);
+
+    it ('should get all products', function () {
+      createController();
+
+      productService.product(function(data){
+        $scope.products = data;
         expect($scope.products.length).toBe(2);
-        expect(productService.addToCart.calls.count()).toBe(1);
+      });
     });
 });
