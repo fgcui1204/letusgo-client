08690ed6b427436c37a6ceb2c77e92f26955641b
fgcui <fgcui1204@163.com>
2014-10-08 21:53:02 +0800

    product init

--- test/spec/controllers/productSpec.js
+++ test/spec/controllers/productSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-xdescribe('productCtrl',function(){
+describe('productCtrl',function(){
     var $scope,productService,createController,products,$controller;
     beforeEach(function () {
         module('letusgo');
@@ -17,9 +17,8 @@ xdescribe('productCtrl',function(){
             });
           };
       products = [
-        {barcode: '1', productSort: {sid: '1', sname: '水果'}, productName: '苹果', productPrice: '10', productUnit: '千克'},
-        {barcode: '2', productSort: {sid: '1', sname: '水果'}, productName: '香蕉', productPrice: '5', productUnit: '千克'},
-        {barcode: '3', productSort: {sid: '2', sname: '饮料'}, productName: '可乐', productPrice: '5', productUnit: '瓶'}
+        {barcode: '4', category: {id: '2', name: '饮料'}, name: '雪碧', price: '3', unit: '瓶'},
+        {barcode: '5', category: {id: '3', name: '服装'}, name: 'NIKE鞋', price: '300', unit: '双'}
       ];
     });
     it ('setToLocal should be execute', function () {
