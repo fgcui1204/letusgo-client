300b1ad11c56ab2f96f7b58afcd70cbc7bf7e250
fgcui <fgcui1204@163.com>
2014-10-08 23:45:37 +0800

    test get all product

--- test/spec/controllers/productManagerSpec.js
+++ test/spec/controllers/productManagerSpec.js
@@ -1,39 +1,45 @@
 'use strict';
 
-xdescribe('ProductManagerCtrl',function() {
-  var $scope, fromLocal, productManagerService,$location, createController,allProduct;
+describe('ProductManagerCtrl',function() {
+  var $scope, productManagerService,$location, createController,products,$controller;
   beforeEach(function () {
     module('letusgo');
     inject(function ($injector) {
+
       $scope = $injector.get('$rootScope').$new();
-      fromLocal = $injector.get('fromLocal');
       productManagerService = $injector.get('productManagerService');
       $location = $injector.get('$location');
-      var $controller = $injector.get('$controller');
+      $controller = $injector.get('$controller');
 
       createController = function () {
         return $controller('ProductManagerCtrl', {
           $scope: $scope,
-          fromLocal: fromLocal,
           $location:$location,
           productManagerService: productManagerService
         });
       };
 
     });
-    allProduct = [
-      {productSort:'水果',productName:'苹果',productPrice:'10',productUnit:'千克'},
-      {productSort:'水果',productName:'香蕉',productPrice:'5',productUnit:'千克'}
+    products = [
+      {barcode: '4', category: {id: '2', name: '饮料'}, name: '雪碧', price: '3', unit: '瓶'},
+      {barcode: '5', category: {id: '3', name: '服装'}, name: 'NIKE鞋', price: '300', unit: '双'}
     ];
+
+    spyOn(productManagerService, 'product').and.callFake(function (callback) {
+      callback(products);
+    });
   });
 
-  it ('it should load all product', function () {
-    spyOn(fromLocal,'getData').and.returnValue(allProduct);
+  it ('it should get all products', function () {
     createController();
-    expect($scope.products).toEqual(allProduct);
+
+    productManagerService.product(function(data){
+      $scope.products = data;
+      expect($scope.products).toEqual(products);
+    });
   });
 
-  it ('it should delete the product', function () {
+  xit ('it should delete the product', function () {
     spyOn(productManagerService,'delete').and.returnValue(allProduct);
     spyOn(fromLocal,'getData').and.returnValue(allProduct);
     createController();
@@ -42,13 +48,13 @@ xdescribe('ProductManagerCtrl',function() {
     expect(productManagerService.delete).toHaveBeenCalledWith(pname);
   });
 
-  it('should come into addProduct when click the add product', function () {
+  xit('should come into addProduct when click the add product', function () {
     createController();
     $scope.toAdd();
     expect($location.path() === '/addProduct').toBe(true);
   });
 
-  it('should come into update when click the update button', function () {
+  xit('should come into update when click the update button', function () {
     var product = {productSort:'水果',productName:'香蕉',productPrice:'5',productUnit:'千克'};
     createController();
     $scope.toUpdate(product);
