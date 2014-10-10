f3bd3c1c6f1c50ec04cd88308d5b0445ff67c61c
fgcui <fgcui1204@163.com>
2014-10-10 15:43:14 +0800

    format code

--- app/scripts/controllers/addProduct.js
+++ app/scripts/controllers/addProduct.js
@@ -1,10 +1,13 @@
 'use strict';
 angular.module('letusgo')
   .controller('addProductCtrl', function ($scope, productManagerService, CategoryManagerService , $location) {
+
     CategoryManagerService .getCategories(function (data) {
       $scope.categories = data;
     });
+
     $scope.productInfo = productManagerService.productInfo();
+
     $scope.addProduct = function () {
       var productInfo = $scope.productInfo;
       productManagerService.addProduct(productInfo, function (data) {
