0322be76c2ec38c2f2d6b4a3f76fe632d65e3cd2
fgcui <fgcui1204@163.com>
2014-10-12 00:00:35 +0800

    refactor code

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -2,26 +2,33 @@
 angular.module('letusgo')
   .controller('CartCtrl', function ($scope,cartService, productService) {
 
+    function initTotalCount(){
+      productService.getTotalCount(function (data) {
+        $scope.$parent.totalCount = data;
+      });
+    }
+
+    function initTotalMoney(){
+      cartService.getTotalMoney(function (data) {
+        $scope.totalMoney = data;
+      });
+    }
+
+
     productService.cartItem(function (data) {
       $scope.cartItems = data;
     });
-    productService.getTotalCount(function (data) {
-      $scope.$parent.totalCount = data;
-    });
 
-    cartService.getTotalMoney(function (data) {
-      $scope.totalMoney = data;
-    });
+    initTotalCount();
+    initTotalMoney();
 
 
     $scope.changeCount = function (item) {
       cartService.changeCount(item, function () {
-        productService.getTotalCount(function (data) {
-          $scope.$parent.totalCount = data;
-        });
-        cartService.getTotalMoney(function (data) {
-          $scope.totalMoney = data;
-        });
+        initTotalCount();
+        initTotalMoney();
       });
     };
+
+
   });
